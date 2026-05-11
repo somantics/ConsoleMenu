299d06d3d2ff771beeb76b01af46629f7ddfcd86
Amos Johan Persson <amosjohanpersson@gmail.com>
2026-05-11 14:22:36 +0200

    built-in menu type for prompts reading multiple inputs at once

--- ConsoleMenu/menu/PromptArrayMenu.cs
+++ ConsoleMenu/menu/PromptArrayMenu.cs
@@ -1,15 +1,12 @@
-using System;
-
 namespace ConsoleMenu.Menu;
 
-
-public class PromptMenu(string? message, string? prompt, BusinessFunction action) : Menu(message, prompt)
+public class PromptArrayMenu(string? message, string? prompt, BusinessFunctionMultipleInput action) : Menu(message, prompt)
 {
 
     public override void Run(IInputService input, IOutputService output, IMenuClient client)
     {
         output.PrintCommandPrompt(this);
-        if (input.ParseString(out string message))
+        if (input.ParseStringMultiple(out string[] message))
         {
             bool success = action.Invoke(message, out string result);
             output.PrintMessage(result);
